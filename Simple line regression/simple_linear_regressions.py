2032e49f9b4617cf3edbfbd5ac88635890d0b442
Paarth Agrawal <79699903+Light-Y007@users.noreply.github.com>
2021-10-07 20:39:10 +0530

    Data Preprocessing and Simple Lineaar Regression

---
+++ Simple line regression/simple_linear_regressions.py
@@ -0,0 +1,82 @@
+#Simple Linear Regression Model
+
+#Data Preprocessing
+
+#importing Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#IMPORTING THE DATASET
+
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+#Taking care of missing data
+#from sklearn.impute import SimpleImputer
+#from sklearn.preprocessing import OneHotEncoder , LabelEncoder
+#imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+#imputer = imputer.fit(X[:, 1:3])
+#X[:,1:3] = imputer.transform(X[:,1:3])
+
+#Encoding categorical data
+#from sklearn.compose import ColumnTransformer
+#ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
+#X = ct.fit_transform(X)
+#labelencoder_y = LabelEncoder()
+#y = labelencoder_y.fit_transform(y)
+
+#Splitting the data into training set and test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
+
+#feature Scaling
+'''from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)'''
+#fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predict the test set results
+y_pred = regressor.predict(X_test)
+
+#Visualising the Training set results
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Trainins Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Test set results
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
